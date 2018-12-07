f7c020b4b9de27ed4dbd958848264f903a06da3a
serinir <imadom1@live.fr>
2018-12-07 14:09:03 +0100

    added function creat elem en fonction d'une inpute

--- function.c
+++ function.c
@@ -4,7 +4,7 @@
 
 #include "headers/function.h"
 
-
+//----------FONCTIONS DE LA FILE---------------//
 void Enfiler(File *f,process x){
   elmFile *p;
   p=(elmFile*)calloc(1,sizeof(elmFile));
@@ -35,4 +35,37 @@ int Filevide(File f){
 
 elmFile  Tetefile(file f){
     return (f.h)->data;
+}
+//------------------FONCTION DE LA PILE--------------//
+memo creat_Partitions(int nombre_de_partitions)
+{
+    memo l,p;
+    int temp;
+    l=malloc(sizeof(partition));
+    p=l;
+    //l'adress de debut de la premiere partition est egal a 0
+    p->data.start=0;
+    // la variable temp va sommer la taille de chaque partition
+    //et la mettre dans l'adress de debut de a prochaine partition
+    scanf("%d",&l->data.size);
+    temp=p->data.size;
+
+    while(nombre_de_partitions--)
+    {
+        p->next=malloc(sizeof(partition));
+        p=p->next;
+        p->data.start=temp;
+        puts("donnée la taille de chaque paritions en kb");
+        do
+        {
+            scanf("%d",&p->data.size);
+            if(p->data.size>1024000)
+                puts("La taille d'une partition ne peut pas dépasser 1024Mo");
+            else if(p->data.size<0)
+                puts("La taille d'une partition ne peut pas etre inferieur a 0kb");
+        }while(p->data.size>1024000 || p->data.size<0);
+        temp+=p->data.size;
+    }
+    p->next=NULL;
+    return l;
 }
